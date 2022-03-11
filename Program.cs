3eabe14e03823b9c9b6c134542018457bf0710c9
milad <miladjalali1384@gmail.com>
2022-03-12 01:25:46 +0800

    3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int a;
+for (int i = 0; i <4;i++)
+{
+    System.Console.WriteLine("hogog bede");
+    a=Convert.ToInt32(Console.ReadLine());
+    if (a>2000000 && a<3000000)
+    {
+        System.Console.WriteLine("maliat"+(a*0.05));
+
+    }
+    else if(a>3000000)
+    {
+        System.Console.WriteLine("maliat"+(a*0.07));
+
+    } else
+    {
+        System.Console.WriteLine("maliat 0");
+    }
+    
+
+}
